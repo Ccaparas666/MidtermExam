59ff5af03f924425167f6116e286d7ef9dba2810
kristine <kristineintol@gmail.com>
2023-03-25 11:59:23 +0800

    THIS IS MY MIDTERM

---
+++ src/app/index-page/index-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-index-page',
+  templateUrl: './index-page.page.html',
+  styleUrls: ['./index-page.page.scss'],
+})
+export class IndexPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
